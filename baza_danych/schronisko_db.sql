67919b3e6f9cf7b9bd3a90dab1a6f9fabd0ec6b1
ZuzannaCh <zuzia.chaniewska@gmail.com>
2026-06-04 23:47:48 +0200

    ładowanie galerii z bazy danych

--- baza_danych/schronisko_db.sql
+++ baza_danych/schronisko_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Cze 04, 2026 at 09:57 PM
+-- Generation Time: Cze 04, 2026 at 11:46 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.0.30
 
@@ -40,6 +40,34 @@ CREATE TABLE `admin_users` (
 INSERT INTO `admin_users` (`id`, `email`, `pass`) VALUES
 (1, 'admin@example.com', '$2y$10$N0xy3BqyCR0Nu9tWexnnhOba3oO3dMtZ.mShddXyGqXLd0WDhqoXu');
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `alt` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `photos`
+--
+
+INSERT INTO `photos` (`id`, `name`, `alt`, `category`) VALUES
+(1, 'nocleg.png', 'zdjęcie noclegu', 'schronisko'),
+(2, 'schronisko_zewn.jpg', 'zdjęcie schroniska', 'schronisko'),
+(3, 'stolowka.png', 'zdjęcie stołówki', 'schronisko'),
+(4, 'szalas.jpg', 'szałas', 'szalas'),
+(5, 'szalas_grill.png', 'grill w szałasie', 'szalas'),
+(6, 'szalas_wnetrze.png', 'wnętrze szałasu', 'szalas'),
+(7, 'szlak.png', 'zdjęcie szlaku', 'okolica'),
+(8, 'wodospad_zdj.png', 'wodospad', 'okolica'),
+(9, 'szlak2.jpg', 'zdjęcie szlaku', 'okolica');
+
 --
 -- Indeksy dla zrzutów tabel
 --
@@ -52,6 +80,12 @@ ALTER TABLE `admin_users`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indeksy dla tabeli `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +94,12 @@ ALTER TABLE `admin_users`
 --
 ALTER TABLE `admin_users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `photos`
+--
+ALTER TABLE `photos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- pages/galeria.php
+++ pages/galeria.php
@@ -1,37 +1,43 @@
+<?php
+
+/** @var mysqli $conn */
+$sql = "
+SELECT
+    id,
+    name,
+    alt,
+    category
+FROM photos
+ORDER BY category
+";
+$result = $conn->query($sql);
+
+$gallery = [];
+while ($row = $result->fetch_assoc()) {
+    $gallery[$row['category']][] = $row;
+}
+?>
+
+
 <section>
     <h1>Galeria</h1>
-    <section>
-        <div class="galeria">
-            <h2>Schronisko</h2>
-            <div class="zdjecia">
-                <img src="img/schronisko_zewn.jpg" alt="Schronisko zewnętrzne">
-                <img src="img/nocleg.png" alt="Pokój noclegowy">
-                <img src="img/stolowka.png" alt="Jadalnia">
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="galeria">
-            <h2>Szałas Sielanka</h2>
-            <div class="zdjecia">
-                <img src="img/szalas.jpg" alt="Szałas zewnętrzny">
-                <img src="img/szalas_wnetrze.png" alt="Wnętrze szałasu">
-                <img src="img/szalas_grill.png" alt="Grill przy szałasie">
-            </div>
-        </div>
-    </section>
-    <section>
-        <div class="galeria">
-            <h2>Okolica</h2>
-            <div class="zdjecia">
-                <img src="img/szlak.png" alt="Szlak górski">
-                <img src="img/wodospad_zdj.png" alt="Wodospad">
-                <img src="img/szlak2.jpg" alt="Ścieżka w lesie">
-            </div>
-        </div>
-    </section>
-</section>
+    <?php foreach ($gallery as $category => $photos): ?>
+        <section>
+            <div class="galeria">
+                <h2><?= htmlspecialchars(ucfirst($category)) ?></h2>
+                <div class="zdjecia">
+                    <?php foreach ($photos as $photo): ?>
+                        <img
+                            src="img/galeria/<?= htmlspecialchars($category) ?>/<?= htmlspecialchars($photo['name']) ?>"
+                            alt="<?= htmlspecialchars($photo['alt']) ?>">
 
-<div id="lightbox">
-    <img id="lightbox-img" alt="Podgląd zdjęcia">
-</div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+    <?php endforeach; ?>
+
+
+    <div id="lightbox">
+        <img id="lightbox-img" alt="Podgląd zdjęcia">
+    </div>
